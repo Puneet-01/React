8a65f5bd138537d56c9b77cafdc63e317853de45
Puneet Tiwari <puneet.tiwari.2023@gmail.com>
2021-10-06 00:02:47 +0530

    Redux Thunk

--- learning/src/redux/configureStore.js
+++ learning/src/redux/configureStore.js
@@ -1,9 +1,11 @@
 
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Comments } from "./Comments";
 import { Leaders } from "./Leaders";
 import { Dishes } from "./Dishes";
 import { Promotions } from "./promotions";
+import thunk from 'redux-thunk';
+import logger from 'redux-thunk';
 
 export const configureStore = () =>{
     const store = createStore(
@@ -12,7 +14,8 @@ export const configureStore = () =>{
             promotions:Promotions,
             leaders:Leaders,
             comments:Comments
-        })
+        }),
+        applyMiddleware(thunk,logger)
         );
     return store;
 }
